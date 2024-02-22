5612942e5911a1ca55f772c97aae548c4e4e9072
chiahsiu0801 <zyzhangtom870801@gmail.com>
2024-02-22 16:17:58 +0800

    modified:   src/app/blog/[slug]/page.tsx new file:   src/app/blog/[slug]/singlePost.module.css new file:   src/components/footer/footer.module.css

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,36 @@
+import Image from 'next/image';
+import styles from './singlePost.module.css';
+
 export default function SinglePostPage() {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/8123311/pexels-photo-8123311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+        fill
+        className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image src="https://images.pexels.com/photos/8123311/pexels-photo-8123311.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          width={50}
+          height={50}
+          className={styles.avatar} />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Chiahsiu Chang</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus incidunt blanditiis aspernatur repudiandae maiores error est eius maxime vitae sint, soluta modi vel consequatur consectetur. Perferendis blanditiis pariatur ad quaerat?
+        </div>
+      </div>
+    </div>
   );
 }
